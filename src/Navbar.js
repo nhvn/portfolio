631ad70dc071e8af285d215f9e390c393f63b14b
Alan <alannhan@ymail.com>
2024-01-31 20:35:45 -0800

    Created Navbar.js file

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>TESTING</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Projects</a>
+                <a href="/">Resume</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
