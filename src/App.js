81fa57f14cd9b5e05c5b9862732929735b6bb1ba
Alan <alannhan@ymail.com>
2024-02-01 22:32:12 -0800

    Created content on home page and fixed nightmode bugs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-// App.js
 import Navbar from './Navbar.js';
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider, theme, GlobalStyle } from './ThemeProvider';
-import AboutPage from './AboutPage';
-import ProjectsPage from './ProjectsPage';
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50vh;
-`;
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -25,17 +14,47 @@ function App() {
     <ThemeProvider theme={isDarkMode ? theme.dark : theme.light}>
       <GlobalStyle />
       <Router>
-        <div className="App">
-          <Navbar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
-          <Routes>
-            <Route path="/" element={
-              <Wrapper>
-                <h1>Hello there!</h1>
-              </Wrapper>
-            } />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-          </Routes>
+      <Navbar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+        <div className="home fade">
+            <div className="flex">
+              <div className="left">
+                <section class="title">
+                  <div class="description">
+                    <p class="small-paragraph">
+                      HELLO THERE!
+                    </p>
+                    <h1>
+                      I'm Alan.
+                    </h1>
+                    <div class="divider"></div>
+                    <div class="description descr-contain">
+                      <div class="text-contain">
+                        <p>I am a dedicated <span class="highlight">full-stack software engineer</span> who's skilled in frontend and backend technologies. My work revolves around creating impactful software solutions, with a keen focus on user experience.</p>
+                        <p>Currently, I'm expanding my knowledge in the fields of Machine Learning and Artificial Intelligence, while actively seeking new opportunities to apply and grow my skillset.</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+              <div className="right">
+                <div class="contact">
+                  <div class="contact-contain">
+                    <h2>Let's chat!</h2>
+                    <div class="contact-list">
+                    <a href="https://www.linkedin.com/in/alannhans" target="_blank">
+                      <img src="/img/linkedin.png" alt="LinkedIn" />
+                    </a>
+                    <a href="https://github.com/nhvn" target="_blank">
+                      <img src="/img/github.png" alt="GitHub" />
+                    </a>
+                    <a href="mailto: alannhan443@gmail.com" target="_blank">
+                      <img src="/img/email.png" alt="Email" />
+                    </a>
+                  </div>
+                  </div>
+                </div>
+              </div>
+          </div>
         </div>
       </Router>
     </ThemeProvider>
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,12 +4,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import { ThemeProvider, theme } from './ThemeProvider';
 
-const Wrapper = styled.div`
-  align-items: center;
-  justify-content: center;
-  height: 20vh;
-`;
-
 const Logo = styled.img`
   max-width: 10%;
 `;
@@ -21,38 +15,37 @@ const StyledLink = styled(Link)`
 
 const Navbar = ({ toggleDarkMode, isDarkMode }) => {
   return (
-    <Wrapper>
-      <nav className="navbar">
-        <a href="/" className="logo">
-          <Logo src="/img/logo.png" alt="Custom logo" />
-        </a>
-        <div className="links">
-          <ThemeProvider isDarkMode={isDarkMode}>
-            <StyledLink to="/about">About</StyledLink>
-            <StyledLink to="/projects">Projects</StyledLink>
-            <StyledLink to="/">Resume</StyledLink>
-          </ThemeProvider>
-        </div>
-        <div>
-          <ThemeProvider isDarkMode={isDarkMode}>
-            <div>
-              <button
-                onClick={toggleDarkMode}
-                style={{
-                  background: 'none',
-                  border: 'none',
-                  cursor: 'pointer',
-                  fontSize: '24px',
-                  marginLeft: '2em',
-                }}
-              >
-                {React.cloneElement(theme[isDarkMode ? 'dark' : 'light'].icon, { color: theme[isDarkMode ? 'dark' : 'light'].color })}
-              </button>
-            </div>
-          </ThemeProvider>
-        </div>
-      </nav>
-    </Wrapper>
+    <nav className="navbar">
+      <a href="/" className="logo">
+        <Logo src="/img/logo.png" alt="Custom logo" />
+      </a>
+      <div className="links">
+        <ThemeProvider isDarkMode={isDarkMode}>
+          <StyledLink to="/about">About</StyledLink>
+          <StyledLink to="/projects">Projects</StyledLink>
+          <StyledLink to="/">Resume</StyledLink>
+        </ThemeProvider>
+      </div>
+      <div>
+        <ThemeProvider isDarkMode={isDarkMode}>
+          <div>
+            <button
+              onClick={toggleDarkMode}
+              style={{
+                background: 'none',
+                border: 'none',
+                cursor: 'pointer',
+                fontSize: '24px',
+                marginLeft: '2em',
+              }}
+            >
+              {React.cloneElement(theme[isDarkMode ? 'dark' : 'light'].icon, { color: theme[isDarkMode ? 'dark' : 'light'].color })}
+            </button>
+          </div>
+        </ThemeProvider>
+      </div>
+    </nav>
+
   );
 };
 
