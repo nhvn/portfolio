c6411ce6299de722f1455bca0a57c5e3fc0a35a8
Alan <alannhan@ymail.com>
2024-02-04 12:33:20 -0800

    Fixed up footer to have flex ratios

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer>
-      <div class="footer-section">
+      <div class="footer-section footer-section-1">
         <h3>Designed & Developed</h3>
         <h3>by Alan Nhan</h3>
       </div>
-      <hr /> {/* Add this line */}
-      <div class="footer-section">
+      <hr />
+      <div class="footer-section footer-section-2">
         <h3>REACH OUT</h3>
         <ul>
           <li><a href="https://www.linkedin.com/in/alannhans" target="_blank">LinkedIn</a></li>
@@ -18,8 +18,8 @@ function Footer() {
           <li><a href="mailto: alannhan443@gmail.com" target="_blank">Email</a></li>
         </ul>
       </div>
-      <hr /> {/* Add this line */}
-      <div class="footer-section">
+      <hr />
+      <div class="footer-section footer-section-3">
         <h3>QUICK FIND</h3>
         <ul>
             <li><Link to="/about">About</Link></li>
