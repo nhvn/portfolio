dfa34f55b3766490791c17d318127c1987ea0df7
Alan <alannhan@ymail.com>
2024-02-25 18:32:10 -0800

    Added intro section but need to add links soon

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -15,8 +15,10 @@ const AboutPage = () => {
                                     <div className="description descr-contain">
                                         <div className="text-contain">
                                             <p>I am a <span className="highlight">full-stack software engineer</span> with a passion for problem-solving and a versatile skillset. I graduated from <span className="highlight2">UCI</span> with a Bachelor of Science in Natural Sciences and I also finished a Full-Stack Software Development Program at <span className="highlight2">CSULB</span>.</p>
-                                            <p>I have experience in working with teams, prioritizing tasks, and delivering high-quality results in fast-paced environments. I have also developed a keen interest in healthcare operations and procedures, as I was a former dental student and a dental assistant. I switched careers to pursue software engineering and I have successfully completed several projects using various frontend and backend technologies.</p>
+                                            {/* <p>I have experience in working with teams, prioritizing tasks, and delivering high-quality results in fast-paced environments. I have also developed a keen interest in healthcare operations and procedures, as I was a former dental student and a dental assistant. I switched careers to pursue software engineering and I have successfully completed several projects using various frontend and backend technologies.</p> */}
                                             {/* <p>I am always eager to learn new skills and take on new challenges in software development, and I am actively seeking new opportunities to apply and grow my skillset in different roles.</p> */}
+                                            <p>Currently expanding my knowledge in the fields of Machine Learning and Artificial Intelligence, while actively seeking new opportunities to apply and grow my skillset.</p>
+
                                         </div>
                                     </div>
                             </div>
@@ -36,7 +38,7 @@ const AboutPage = () => {
             </div>
             <div className="divider"></div>
             <section className="hobbies">
-                <h2 id="titles">Obsessions</h2>
+                <h2 id="titles">Current obsessions</h2>
                 <div className="hobbies-contain">
                     <div className="hob" id="foodie">
                         <img src={process.env.PUBLIC_URL + '/img/food.png'} alt="Coming Soon"></img>
--- src/HomePage.js
+++ src/HomePage.js
@@ -4,45 +4,8 @@ import Projects from './Projects';
 const HomePage = () => {
   return (
     <div className="home fade">
-      <div className="flex">
-        <div className="left">
-          <section className="title">
-            <div className="description">
-              {/* <h4 className="small-paragraph">
-                HELLO THERE,
-              </h4> */}
-              <div className='center'>
-                <h1><span className='highlight'>Hello there!</span> I'm Alan.</h1>
-                <h1>Software engineer</h1>
-                <h1>Based in Los Angeles.</h1>
-              </div>
-              <div className="description descr-contain">
-                <div className="text-contain">
-                  {/* <p>I am a dedicated <span className="highlight">full-stack software engineer</span> who's skilled in frontend and backend technologies. My work revolves around creating impactful software solutions, with a keen focus on user experience.</p> */}
-                  {/* <p>Currently, I'm expanding my knowledge in the fields of <span className='highlight2'>Machine Learning</span> and <span className='highlight2'>Artificial Intelligence</span>, while actively seeking new opportunities to apply and grow my skillset.</p> */}
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-        {/* <div className="right">
-          <div className="contact">
-            <div className="contact-contain">
-              <h2>Let's chat</h2>
-              <div className="contact-list">
-                <a href="https://www.linkedin.com/in/alannhans" target="_blank" rel="noopener noreferrer">
-                  <img src="/img/linkedin.png" alt="Logo for LinkedIn" />
-                </a>
-                <a href="https://github.com/nhvn" target="_blank" rel="noopener noreferrer">
-                  <img src="/img/github.png" alt="Logo for Github" />
-                </a>
-                <a href="mailto: alannhan443@gmail.com" rel="noopener noreferrer">
-                  <img src="/img/email.png" alt="Logo for email" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div> */}
+      <div className='intro1'>
+        <object type="image/svg+xml" data="img/intro.svg">Your browser does not support SVG</object>
       </div>
       <div className="divider"></div>
       <Projects />
