cc332907688b220e750a74246ed7517c3f490ba4
Alan <alannhan@ymail.com>
2024-02-26 20:22:34 -0800

    Made minor updates to intro cover

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,9 +7,9 @@ const HomePage = () => {
       <div className='intro1'>
         <object type="image/svg+xml" data="img/intro.svg">Your browser does not support SVG</object>
         <div className='intro-text'>
-          <h1><span className='highlight1-1'>Hey there!</span> <span className='highlight1-2'>I'm Alan.</span></h1>
+          <h1><span className='highlight1-1'>Hey there!</span> <span className='highlight1-2'>I'm Alan</span></h1>
           <h1><span className='highlight1-2'>Software Engineer</span></h1>
-          <h1><span className='highlight1-2'>Based in Los Angeles.</span></h1>
+          <h1><span className='highlight1-2'>based in Los Angeles.</span></h1>
         </div>
       </div>
       <div className="divider"></div>
