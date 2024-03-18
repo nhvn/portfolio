ad1234b6cf24fb81d1fcf5f897c34e32e2481c97
Alan <alannhan@ymail.com>
2024-03-17 21:32:57 -0700

    Made lots of visual changes to margins and such. Also made changes to individ proj pages

--- src/ProjPage.js
+++ src/ProjPage.js
@@ -15,11 +15,17 @@ export const projects = [
     team: 'Myself and 2 other software engineers.',
     duration: 'Feb 2024 - Present',
     outcome: 'Project is in progress. Stay tuned for the completed version and the exciting improvements it will bring!',
-    sketchImage: '/img/fitSketch.png',
+    prob: '',
+    sol: '',
+    sketchImage1: '/img/fitSketch.png',
+    sketchImage2: '/img/fitMockup.png',
+    sketchImage3: '',
     sketch: 'For the website, I meticulously examined the layout and envisioned its adaptation for a seamless mobile app experience, basing my insights on the sketches I developed. My goal is to ensure consistency between the web and mobile platforms, making navigation effortless and ensuring all features are readily accessible. This approach is particularly crucial for our fitness application, where user engagement and accessibility are paramount.',
     methodImage: '/img/fitMockup.png',
     method: 'This represents the envisioned design direction for my app. Currently, I am actively developing it using Swift and Xcode to ensure an exceptional user experience. While these designs may evolve, they serve as the foundation for further prototyping. Additionally, we are exploring the integration of these designs with the Apple Watch to expand accessibility and functionality.',
-    resultsImage: '/img/comingSoon.png',
+    resultsImage1: '',
+    resultsImage2: '',
+    resultsImage3: '',
     results: '',
     finalThoughts: "",
     codeLink: 'https://github.com/nhvn/BeFit-v2',
@@ -38,11 +44,17 @@ export const projects = [
     team: 'Myself and two other software engineers.',
     duration: 'Apr 2023 - Jun 2023',
     outcome: 'We developed a To-Do List App that allows users to perform all these actions. The app also provides secure user registration, login, and logout features, with tasks stored securely in a PostgreSQL database.',
-    sketchImage: '/img/todoSketch.png',
+    prob: '',
+    sol: '',
+    sketchImage1: '/img/todoSketch.png',
+    sketchImage2: '',
+    sketchImage3: '',
     sketch: 'In the initial design phase, I focused on creating intuitive and user-friendly interfaces. I sketched various layouts, considering factors like usability and aesthetic appeal. The goal was to create a design that would provide a seamless user experience while maintaining a clean and modern look.',
     methodImage: '/img/comingSoon.png',
     method: 'My approach was iterative and user-centric. I started with low-fidelity wireframes, gradually refining them based on user feedback and usability testing. I also considered the latest design trends and best practices, ensuring the design was both contemporary and intuitive.',
-    resultsImage: '/img/comingSoon.png',
+    resultsImage1: '/img/comingSoon.png',
+    resultsImage2: '',
+    resultsImage3: '',
     results: 'The final design effectively balanced functionality and aesthetics. It provided users with a clear and easy-to-navigate interface, improving task management efficiency. The design was well-received by users, demonstrating the effectiveness of the user-centric design approach.',
     finalThoughts: 'This project reinforced the importance of user-centric design and iterative development. It was a rewarding experience that highlighted the impact of good design on user experience. I look forward to applying these insights to future projects.',
     codeLink: 'https://github.com/nhvn/to-do-list',
@@ -61,11 +73,17 @@ export const projects = [
     team: 'Myself.',
     duration: 'Apr 2023 - Jun 2023',
     outcome: 'Successfully created a personal portfolio website that effectively showcases my skills, experiences, and projects. The website has helped in networking and opened up new opportunities.',
-    sketchImage: '/img/portfolioSketch.png',
+    prob: '',
+    sol: '',
+    sketchImage1: '/img/portfolioSketch.png',
+    sketchImage2: '',
+    sketchImage3: '',
     sketch: 'The project sketches visually represent the structure of the website, including the layout of the introduction, about, project, and contact sections. They also highlight the design elements of the navigation and footer. In addition, they depict how responsive design has been implemented to ensure a seamless user experience across different devices. Each sketch offers a clear depiction of the main components and their interplay, which enhances the understanding of the website’s design and functionality.',
     methodImage: '/img/methodOldportfolio.png',
     method: 'The project started with a design phase where the layout of the website was planned. The development phase involved coding the website using HTML, CSS, JavaScript, and Bootstrap. A contact form was integrated for easy communication. Responsive design principles were implemented to ensure the website looks good on all devices.',
-    resultsImage: '/img/resultsOldportfolio.png',
+    resultsImage1: '/img/resultsOldportfolio.png',
+    resultsImage2: '',
+    resultsImage3: '',
     results: 'The portfolio website project yielded positive results. The website effectively showcases my skills, experiences, and projects, providing visitors with a comprehensive view of my capabilities. The responsive design ensures a seamless experience across different devices.',
     finalThoughts: 'This is my first personal portfolio that I have ever made, after completing my program. It not only allowed me to showcase my skills and experiences but also gave me a platform to share my work with others. The project was a great opportunity to apply my full stack development skills in a real-world scenario.',
     codeLink: 'https://github.com/nhvn/nhvn.github.io',
@@ -84,11 +102,17 @@ export const projects = [
     team: 'Myself, another software engineer, and an electronics engineer.',
     duration: 'Feb 2023 - Mar 2023',
     outcome: 'Developed a Smart Lighting Control System that allows precise control of lights. The system also provides customizable schedules for each light.',
-    sketchImage: '/img/lightSketch.png',
+    prob: '',
+    sol: '',
+    sketchImage1: '/img/lightSketch.png',
+    sketchImage2: '/img/lightSketch.png',
+    sketchImage3: '/img/lightSketch.png',
     sketch: 'These sketches illustrate the overall system design, the setup of the Raspberry Pi and its GPIO pin configuration, the process of controlling the lights, the setup of the Flask web server, the design of the user interface, the implementation of scheduling, and the use of multithreading for parallel light control. Each sketch focuses on a key aspect of the project, providing a clear visual representation of the main components and their interactions, making the project’s workings easier to understand.',
     methodImage: '/img/raspPi.png',
     method: 'In this project, we started with a high-level system design that included the Raspberry Pi, the lights, and the user interface. We set up the Raspberry Pi with the necessary software and used its GPIO pins to control the lights. The lights were controlled using signals from the Raspberry Pi. We then set up a Flask web server to handle requests and control the lights. The user interface was designed with main screens for light control and scheduling. Scheduling was implemented to allow users to set schedules for controlling the lights. We used multithreading to enable parallel control of lights, with each thread controlling a different light. Throughout the project, we used Python, Flask, and the Raspberry Pi’s GPIO pins. We also faced and overcame various challenges, demonstrating our problem-solving skills and technical expertise.',
-    resultsImage: '/img/resultLight.png',
+    resultsImage1: '/img/resultLight.png',
+    resultsImage2: '',
+    resultsImage3: '',
     results: 'The Smart Lighting Control System project yielded impressive results. The system successfully enabled precise control of up to 50 lights across 10 rooms, with customizable schedules for each light. The use of Python, Flask, and Raspberry Pi’s GPIO pins allowed for efficient and effective control of the lighting system. The implementation of multithreading ensured parallel control of lights, enhancing the system’s performance. The user-friendly interface facilitated easy management and scheduling of lights. Overall, the project demonstrated the potential of IoT in improving home automation and energy efficiency.',
     finalThoughts: "This project was a fascinating journey into the world of IoT and automation. It underscored the transformative potential of technology in everyday life. The ability to control lighting across multiple rooms with customizable schedules not only enhanced convenience but also promoted energy efficiency. The project was a testament to the power of Python and Flask in creating robust and scalable solutions. It was a rewarding experience that highlighted the synergy between software and hardware in creating impactful solutions. I look forward to leveraging these insights in future IoT projects.",
     codeLink: '',
@@ -107,11 +131,17 @@ export const projects = [
     team: 'Myself.',
     duration: 'Oct 2022 - Feb 2023',
     outcome: 'I developed ‘Pumpkin Lostte’, where players navigate through barriers to rescue a lost pumpkin, adding an actual destination to reach, unlike the endless gameplay of ‘Flappy Bird’.',
-    sketchImage: '/img/pumpkinSketch.png',
+    prob: '',
+    sol: '',
+    sketchImage1: '/img/pumpkinSketch.png',
+    sketchImage2: '',
+    sketchImage3: '',
     sketch: 'The game design process was centered around crafting an immersive gameplay experience. The focus was on the game mechanics, where the player would have to navigate through a series of hurdles. The player character was constantly pursued, adding a layer of thrill and urgency to the game. This chase element was integral to the gameplay, challenging the player’s skills and decision-making in real-time. The design sketches reflected these mechanics, illustrating the dynamic and interactive nature of the game.',
     methodImage: '/img/pumpkinMethod.png',
     method: 'The development process was iterative, beginning with a basic game loop and progressively incorporating features such as barrier navigation and game over conditions. A significant focus was placed on the individual designs of the game elements. The character “Jerry”, the “Pumpkin”, the “Chaser”, and the “Black Barriers” were all meticulously designed to enhance the gameplay experience. The designs for the “Chaser” and “Black Barriers” were particularly detailed, crafted carefully on my notes app. User feedback was integrated at each stage, further refining the game and enriching the player’s experience.',
-    resultsImage: '/img/pumpkinResult.png',
+    resultsImage1: '/img/pumpkinResult.png',
+    resultsImage2: '',
+    resultsImage3: '',
     results: 'The final game provides a challenging yet enjoyable gaming experience. The unique twist of having an actual destination adds a new dimension to the gameplay, making it stand out from similar games. The Halloween-themed design was well-received by players, adding to the game’s appeal.',
     finalThoughts: 'This project is my first milestone project from my bootcamp program, and I am proud of what I have accomplished so far. This is a JavaScript browser game built with HTML and CSS. I created this project around Halloween, so I decided to make it Halloween-themed. I designed some elements of the game from scratch, like the black barriers that players should avoid touching, or they lose. The game has room for improvement, but considering my knowledge at the time, I think it is awesome. Please feel free to try it out if you dare. It is a challenging game, so do not hesitate to give it multiple attempts.',
     codeLink: 'https://github.com/nhvn/first-browser-game',
@@ -161,8 +191,8 @@ function ProjPage() {
               </section>
             </div>
             <div className="right">
-              <div className="contact">
-                <div className="proj-contain">
+              <div className="proj-pic">
+                <div>
                   <img src={project.image} alt={project.title} />
                 </div>
               </div>
@@ -193,53 +223,54 @@ function ProjPage() {
         <div className="divider"></div>
         {/* BOTTOM PORTION */}
         <div className="proj-content-container">
-          <div className='prob'>
-            <h2>Problem</h2>
-            <div className='descr'>
-              <p>This is a problem. And this is the description of the problem.</p>
-            </div>
-          </div>
-          <div className='prob'>
-            <h2>Solution</h2>
-            <div className='descr'>
-              <p>This is the solution. Woohoo!</p>
-            </div>
-          </div>
-          {project.sketch && (
-            <div className="flex">
-              <div className="left-proj">
-                <img className="botImg" src={project.sketchImage} alt={project.title} />
+          {(project.prob || project.sol) && (
+            <>
+              <div className='prob'>
+                <h2>Problem</h2>
+                <div className='descr'>
+                  <p>{project.prob}</p>
+                </div>
               </div>
-              <div className='right-proj'>
-                <h2>Sketches</h2>
-                <p>{project.sketch}</p>
+              <div className='prob'>
+                <h2>Solution</h2>
+                <div className='descr'>
+                  <p>{project.sol}</p>
+                </div>
+              </div>
+            </>
+          )}
+          {/* <div className="divider"></div> */}
+          {project.sketch && (
+            <div className="projPics-container">
+              <h2>Sketches</h2>
+              <p>{project.sketch}</p>
+              <div className="projPics">
+                {/* temporarily not using alt='' */}
+                <img className="botImg" src={project.sketchImage1} alt=''/>
+                <img className="botImg" src={project.sketchImage2} alt=''/>
+                {/* <img className="botImg" src={project.sketchImage3} alt=''/> */}
               </div>
             </div>
           )}
           {project.sketch && project.method && (
             <>
               {/* <div className="divider"></div> */}
-              <div className="flex">
-                <div className="right-proj">
-                  <h2>Methodology</h2>
-                  <p>{project.method}</p>
-                </div>
-                <div className='left-proj'>
-                  <img className="botImg" id="rightImg" src={project.methodImage} alt={project.title} />
-                </div>
+              <div className="projPics-container">
+                <h2>Methodology</h2>
+                <p>{project.method}</p>
               </div>
             </>
           )}
           {project.sketch && project.method && project.results && (
             <>
               {/* <div className="divider"></div> */}
-              <div className="flex">
-                <div className="left-proj">
-                  <img className="botImg" src={project.resultsImage} alt={project.title} />
-                </div>
-                <div className='right-proj'>
-                  <h2>Results</h2>
-                  <p>{project.results}</p>
+              <div className="projPics-container">
+                <h2>Results</h2>
+                <p>{project.results}</p>
+                <div className="projPics">
+                  <img className="botImg" src={project.resultsImage1} alt=''/>
+                  <img className="botImg" src={project.resultsImage2} alt=''/>
+                  {/* <img className="botImg" src={project.resultsImage3} alt=''/> */}
                 </div>
               </div>
             </>
@@ -247,25 +278,21 @@ function ProjPage() {
           {project.sketch && project.method && project.results && project.finalThoughts && (
             <>
               {/* <div className="divider"></div> */}
-              <div className="flex">
-                <div className='right-proj finalThoughts'>
-                  <div className='final'>
-                    <h2>Final Thoughts</h2>
-                    <p>{project.finalThoughts}</p>
-                  </div>
-                </div>
+              <div className='final'>
+                <h2>Final Thoughts</h2>
+                <p>{project.finalThoughts}</p>
               </div>
             </>
           )}
           {(!project.sketch || !project.method || !project.results || !project.finalThoughts) && (
             <div className="flex">
-              <div className='right-proj tuned'>
+              <div className='tuned'>
                 {/* <h2>Upcoming</h2> */}
                 <p id='stay-tuned'>More exciting details are on the way. Stay tuned!</p>
               </div>
             </div>
-        )}
-      </div>
+          )}
+        </div>
     </div>
   </div>
   );
