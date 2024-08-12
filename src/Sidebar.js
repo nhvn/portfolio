2ea5b4a03ce4e7fe074082b06d7878f44ff408f9
Alan <alannhan@ymail.com>
2024-08-11 18:02:49 -0700

    Added mail to side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaYoutube, FaTiktok, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaYoutube, FaTiktok, FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import { SiX } from 'react-icons/si';
 import './index.css';
 
@@ -12,6 +12,7 @@ const Sidebar = () => {
         <li><a href="https://www.tiktok.com/@totezalan?is_from_webapp=1&sender_device=pc" target="_blank" rel="noreferrer"><FaTiktok /></a></li>
         {/* <li><a href="https://www.youtube.com/channel/UCeq4B3hUh6C-sDbvtQPiwjA" target="_blank" rel="noreferrer"><FaYoutube /></a></li> */}
         <li><a href="https://x.com/totezalan" target="_blank" rel="noreferrer"><SiX /></a></li>
+        <li><a href="mailto:alannhan443@gmail.com" target="_blank" rel="noreferrer"><FaEnvelope /></a></li>
       </ul>
     </div>
   );
