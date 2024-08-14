2b81657a56322028f335a7830619020ee4f93554
Alan <alannhan@ymail.com>
2024-08-13 19:57:13 -0700

    Added period after Alan for the intro cover

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
       <div className='introCover'>
         <object type="image/svg+xml" data="img/IntroNOW.svg">Your browser does not support SVG</object>
         <div className='intro-text'>
-          <h1><span className='highlightIntro'>Hey there!</span> <span className='highlight1-2'>I'm Alan</span></h1>
+          <h1><span className='highlightIntro'>Hey there!</span> <span className='highlight1-2'>I'm Alan.</span></h1>
           <h1><span className='highlightIntro2'>Software Engineer</span></h1>
           <h1><span className='highlightIntro2'>based in Los Angeles.</span></h1>
         </div>
