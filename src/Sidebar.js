309b6fc2061762b9d5207a6353220f36201e7370
Alan <alannhan@ymail.com>
2024-10-03 22:16:16 -0700

    Added more changes to augment project and added youtube back into the picture

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,7 +10,7 @@ const Sidebar = () => {
         <li><a href="https://www.linkedin.com/in/alannhans" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
         <li><a href="https://github.com/nhvn" target="_blank" rel="noreferrer"><FaGithub /></a></li>
         <li><a href="https://www.tiktok.com/@totezalan?is_from_webapp=1&sender_device=pc" target="_blank" rel="noreferrer"><FaTiktok /></a></li>
-        {/* <li><a href="https://www.youtube.com/channel/UCeq4B3hUh6C-sDbvtQPiwjA" target="_blank" rel="noreferrer"><FaYoutube /></a></li> */}
+        <li><a href="https://www.youtube.com/channel/UCeq4B3hUh6C-sDbvtQPiwjA" target="_blank" rel="noreferrer"><FaYoutube /></a></li>
         <li><a href="https://x.com/totezalan" target="_blank" rel="noreferrer"><SiX /></a></li>
         <li><a href="mailto:alannhan443@gmail.com" target="_blank" rel="noreferrer"><FaEnvelope /></a></li>
       </ul>
