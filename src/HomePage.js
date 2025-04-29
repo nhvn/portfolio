8cbad4aa7bd1fd1372293e5d170a8f26326c182c
Alan <alannhan@ymail.com>
2025-04-29 10:37:03 -0700

    Updated home page to intern at HL

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,8 +8,10 @@ const HomePage = () => {
         <object type="image/svg+xml" data="img/IntroNOW.svg">Your browser does not support SVG</object>
         <div className='intro-text'>
           <h1><span className='highlightIntro'>Hey there!</span> <span className='highlight1-2'>I'm Alan.</span></h1>
-          <h1><span className='highlightIntro2'>Software Engineer</span></h1>
-          <h1><span className='highlightIntro2'>based in Los Angeles.</span></h1>
+          <h1><span className='highlightIntro2'>Data Engineer Intern</span></h1>
+          <h1><span className='highlightIntro2'>at Houlihan Lokey.</span></h1>
+          {/* <h1><span className='highlightIntro2'>Software Engineer</span></h1>
+          <h1><span className='highlightIntro2'>based in Los Angeles.</span></h1> */}
         </div>
       </div>
       <div className="divider"></div>
